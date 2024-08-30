962e46cad55f1ce199729ab9648bec72747bd2ef
Daniel Gonçalves Batista <88516108+danielgbatista@users.noreply.github.com>
2024-08-30 09:41:07 -0300

    :wrench: add alias pathnames

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@atoms': path.resolve(__dirname, 'src/atomic-component/atom'),
+      '@molecules': path.resolve(__dirname, 'src/atomic-component/molecule'),
+      '@organisms': path.resolve(__dirname, 'src/atomic-component/organism'),
+      '@pages': path.resolve(__dirname, 'src/atomic-component/page'),
+    }
+  },
   plugins: [react()],
 })
