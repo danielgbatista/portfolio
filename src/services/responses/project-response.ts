669b607b4eaea0ab1b0b681672097b0f9ea93716
Daniel Gonçalves Batista <88516108+danielgbatista@users.noreply.github.com>
2024-09-03 15:42:24 -0300

    :label: add project response type

---
+++ src/services/responses/project-response.ts
@@ -0,0 +1,11 @@
+export interface ProjectResponse {
+    id: number;
+    title: string;
+    description: string;
+    image: string;
+    technologies: string[];
+    github: string;
+    website: string;
+    createdAt: string;
+    updatedAt: string;
+}
